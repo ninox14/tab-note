dd89b3a3676796eb2adbd3c987bbac6de0e56d96
NiNoX14 <ninox14@pm.me>
2022-09-24 20:13:18 +0300

    feat: radio button component

---
+++ src/components/ui/radio/RadioButton.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react'
+
+interface Props extends React.HTMLProps<HTMLInputElement> {
+  mainColor: string
+  accent?: string
+}
+
+export const RadioButton: FC<Props> = ({ className, mainColor, ...props }) => {
+  return (
+    <label
+      className={`relative h-6 w-6 border-2 rounded-full border-[${mainColor}] ${className}`}
+      style={{ borderColor: mainColor }}
+    >
+      <input type="checkbox" className="hidden peer" {...props} />
+      <span
+        className={`block h-2 w-2 transition-multiple ease-in bg-[${mainColor}]
+        rounded-full peer-checked:bg-accent peer-checked:h-4 peer-checked:w-4
+        group-hover:h-3 group-hover:w-3
+        absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+        `}
+      />
+    </label>
+  )
+}
